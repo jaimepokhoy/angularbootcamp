257c205f403597b18b41157b234d99965d14d192
Salam Al Yahya <salam.al.yahya@avanade.com>
2018-03-27 15:54:02 -0400

    add strike through directive

---
+++ src/app/strike.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appStrike]'
+})
+export class StrikeDirective implements OnChanges {
+  @Input() appStrike: boolean;
+
+  constructor(private el: ElementRef) {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.appStrike.currentValue) {
+      this.el.nativeElement.style.textDecoration = 'line-through';
+    } else {
+      this.el.nativeElement.style.textDecoration = null;
+    }
+  }
+
+}
